9294fe86d3f71bd7b7ef5eb3494a908da0d73d57
Itsme <93462030+sarthak-1996@users.noreply.github.com>
2024-03-13 17:48:12 +0530

    Create putArtifactory.groovy

---
+++ vars/putArtifactory.groovy
@@ -0,0 +1,10 @@
+def call(string jfrogip){
+  withCredentials([(
+        credentialsId: "jfrog",
+        usernameVariable: "USER",
+        passwordVariable: "PASS"
+  )]){
+      sh "curl -X PUT -u ${USER}:${PASS} /var/jenkins/workspace/project/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://${jfrogip}:8082/artifactory/example-repo-local/"
+  }
+  
+}
